fc2570f33b63585e6cd4c70a390ef6bd8adfd345
HengZ121 <hzhan274@uottawa.ca>
2019-10-16 16:59:32 -0400

    Main and Register screens are done

---
+++ Accounts.java
@@ -0,0 +1,11 @@
+/**
+* This class is to use Firebase to implement the function that users can creat accounts
+* There is one and only one admin account (username: admin, pwd:5T5ptQ)
+* Employee and patients can be created infinitly
+* @author Heng(Scott) Zhang (a Universite d'Ottawa, numero etudient: 300067988)
+* Date Started: October 16, 2019
+*/
+
+public class Account{
+
+}
