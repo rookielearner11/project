b745b9626dbe1fb2bc0399b9fc3e40456652e824
HengZ121 <hzhan274@uottawa.ca>
2019-10-17 20:55:23 -0400

    Firebase added

---
+++ Accounts.java
@@ -0,0 +1,33 @@
+/**
+* This class is to use Firebase to implement the function that users can creat accounts
+* There is one and only one admin account (username: admin, pwd:5T5ptQ)
+* Employee and patients can be created infinitly
+* @author Heng(Scott) Zhang (a Universite d'Ottawa, numero etudient: 300067988)
+* Date Started: October 16, 2019
+*/
+
+public class Account{
+	protected String email;
+	protected String password;
+	protected String role;
+	protected String name;
+	final protected Account = Account()
+    public Account(String email, String password, String role, String name){
+    	this.email = email.toUpperCase();
+    	this.password = password;
+    	this.role = role.toUpperCase();
+    	this.name = name;
+    }
+    public String getEmail(){
+    	return this.email;
+    }
+    public String getPwd(){
+    	return this.password;
+    }
+    public String getRole(){
+    	return this.role;
+    }
+    public String getName(){
+    	return this.name;
+    }
+}
