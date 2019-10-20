3511f68275ba63e728b3617433e5cf3c535a8ec2
HengZ121 <hzhan274@uottawa.ca>
2019-10-20 16:59:43 -0400

    Added information to database is finished

--- Clinic/app/src/main/java/com/example/clinic/RegisterActivity.java
+++ Clinic/app/src/main/java/com/example/clinic/RegisterActivity.java
@@ -13,14 +13,21 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RegisterActivity extends AppCompatActivity {
     EditText emailR, pwd1R,pwd2R,nameR,roleR;
     Button btn;
     FirebaseAuth myFirebaseAuth;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         myFirebaseAuth = FirebaseAuth.getInstance();
         nameR = findViewById(R.id.name);
         emailR = findViewById(R.id.email);
@@ -33,11 +40,11 @@ public class RegisterActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = nameR.getText().toString();
-                String email = emailR.getText().toString();
-                String pwd1 = pwd1R.getText().toString();
-                String pwd2 = pwd2R.getText().toString();
-                String role = roleR.getText().toString();
+                final String name = nameR.getText().toString();
+                final String email = emailR.getText().toString();
+                final String pwd1 = pwd1R.getText().toString();
+                final String pwd2 = pwd2R.getText().toString();
+                final String role = roleR.getText().toString();
                 if(pwd1 != pwd2){
                     pwd1R.setError("Passwords you entered are not matched");
                     pwd2R.setError("Passwords you entered are not matched");
@@ -52,8 +59,19 @@ public class RegisterActivity extends AppCompatActivity {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (!task.isSuccessful()){
-                                Toast.makeText(RegisterActivity.this, "Fields Are Empty!", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(RegisterActivity.this, "Error Occoured!", Toast.LENGTH_SHORT).show();
                             }else{
+                                DatabaseReference myRef1 = database.getReference("email");
+                                myRef1.setValue(email);
+
+                                DatabaseReference myRef2 = database.getReference("password");
+                                myRef2.setValue(pwd1);
+
+                                DatabaseReference myRef3 = database.getReference("role");
+                                myRef3.setValue(role);
+
+                                DatabaseReference myRef4 = database.getReference("name");
+                                myRef4.setValue(name);
                                 startActivity(new Intent(RegisterActivity.this, MainActivity.class));
                             }
                         }
