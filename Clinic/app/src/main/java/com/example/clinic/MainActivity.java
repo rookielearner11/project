789d07e5df1121575fbdfa33c9a2847c2c7b8980
HengZ121 <hzhan274@uottawa.ca>
2019-11-09 16:30:48 -0500

    APK is created and admin can log in using its name and pwd

--- Clinic/app/src/main/java/com/example/clinic/MainActivity.java
+++ Clinic/app/src/main/java/com/example/clinic/MainActivity.java
@@ -1,3 +1,11 @@
+/**
+ * This class is to use Firebase to implement the function that users can creat accounts
+ * There is one and only one admin account (username: admin, pwd:5T5ptQ)
+ * Employee and patients can be created infinitly
+ * @author Heng(Scott) Zhang (a Universite d'Ottawa, numero etudient: 300067988)
+ * Date Started: October 16, 2019
+ */
+
 package com.example.clinic;
 
 import androidx.annotation.NonNull;
@@ -6,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.hardware.biometrics.BiometricPrompt;
 import android.os.Bundle;
+import android.sax.StartElementListener;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
@@ -54,10 +63,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String eml = email.getText().toString();
                 String pw = pwd.getText().toString();
-                if (pw.isEmpty()|| eml.isEmpty()){
+                if (pw.isEmpty()|| eml.isEmpty()) {
                     pwd.setError("You must enter all the information required");
                     email.setError("You must enter all the information required");
-                } else if (!pw.isEmpty()&& !eml.isEmpty()){
+                } else if (eml.equals("admin") && pw.equals("5T5ptQ") ){
+                    startActivity( new Intent(MainActivity.this, WelcomeActivity.class));
+                } else if (!pw.isEmpty() && !eml.isEmpty()){
                     myFirebaseAuth.signInWithEmailAndPassword(eml,pw).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>(){
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
