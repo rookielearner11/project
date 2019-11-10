983c946c68ca81c033f00027fa81e67ed92076bc
HengZ121 <hzhan274@uottawa.ca>
2019-11-10 18:26:52 -0500

    Now admin can log out

--- Clinic/app/src/main/java/com/example/clinic/Admin.java
+++ Clinic/app/src/main/java/com/example/clinic/Admin.java
@@ -2,7 +2,10 @@ package com.example.clinic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Admin extends AppCompatActivity {
@@ -18,5 +21,12 @@ public class Admin extends AppCompatActivity {
             text1.setText("Hello administrator");
             text2.setText("You have signed in as administrator");
         }
+        final Button btn_logout= findViewById(R.id.btn_signout);
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Admin.this, MainActivity.class));
+            }
+        });
     }
 }
